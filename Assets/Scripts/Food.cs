cf2cb442d9618eb24a642168004a7ea966747f8e
trbp-lost <vinotengku@gmail.com>
2023-12-24 13:33:18 +0700

    Add Scene and Update Food.cs
    + food.cs full run all program when in scene no8

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,29 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Food : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Scene currentScene = SceneManager.GetActiveScene();
         FoodSpawn spawn = GameObject.Find("FoodSpawn").GetComponent<FoodSpawn>();
+        
+        if (currentScene.name != "no8") return;
+
         Score score = GameObject.Find("Canvas/Panel/TextScore").GetComponent<Score>();
 
         if (spawn != null && collision.collider.tag == "Player")
         {
+
             Destroy(gameObject);
             score.AddScore(1);
             spawn.StartSpawn(1, 3);
 
-            //Vector3 randomPosition = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0);
-            //GameObject newitem = Instantiate(gameObject, randomPosition, Quaternion.identity);
-            //newitem.GetComponent<Collider2D>().enabled = true;
-            //newitem.name = "Food (Clone)";
-
-
         }
     }
-
-
 
 }
